3239ee47c9ab277ae4f36e148fab427ce9fa6a91
Kelly Lutz <kellylutz@Kellys-MacBook-Air.local>
2024-06-06 15:00:02 -0600

    add interactviity

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+function bigImg(x) {
+    x.style.height = "75px";
+    x.style.width = "75px";
+ }
+ 
+ function normalImg(x) {
+    x.style.height = "50px";
+    x.style.width = "50px";
+ }
+
+ function bigFont(x) {
+    x.style.fontSize = "1.5rem";
+ }
+
+ function normalFont(x) {
+    x.style.fontSize = "1rem";
+ }
